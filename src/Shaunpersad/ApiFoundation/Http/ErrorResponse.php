07e66d7785d18b89fc05c7bb36139c5b8950ede5
Shaun Persad <shaun.persad@domandtom.com>
2014-06-25 10:42:03 -0400

    Added more arguments to Error and Success responses to match the underlying Response::json call

--- src/Shaunpersad/ApiFoundation/Http/ErrorResponse.php
+++ src/Shaunpersad/ApiFoundation/Http/ErrorResponse.php
@@ -6,7 +6,7 @@ use Response;
 
 class ErrorResponse {
 
-    public static function make( $message = '', $status = 200, array $headers = array() ) {
+    public static function make( $message = '', $status = 200, array $headers = array(), $options = 0 ) {
 
         $response = App::make(
             'api_response_array',
@@ -16,6 +16,7 @@ class ErrorResponse {
                 'error_description' => $message
             )
         );
-        return Response::json($response, $status);
+
+        return Response::json($response, $status, $headers, $options);
     }
 } 
--- src/Shaunpersad/ApiFoundation/Http/SuccessResponse.php
+++ src/Shaunpersad/ApiFoundation/Http/SuccessResponse.php
@@ -6,7 +6,7 @@ use Response;
 
 class SuccessResponse {
 
-    public static function make($object) {
+    public static function make($object, $status = 200, array $headers = array(), $options = 0 ) {
 
         $response = App::make(
             'api_response_array',
@@ -15,6 +15,6 @@ class SuccessResponse {
             )
         );
 
-        return Response::json($response, 200);
+        return Response::json($response, $status, $headers, $options);
     }
 } 
